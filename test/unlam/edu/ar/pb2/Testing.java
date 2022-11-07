d750fee60ec78ec7e3e988b07faa231378c13ad0
Luca <lucamodic17@gmail.com>
2022-11-07 11:13:03 -0300

    queLasFiguritasAgregadasDeFormaDesordenadaQuedenOrdenadas()

--- test/unlam/edu/ar/pb2/Testing.java
+++ test/unlam/edu/ar/pb2/Testing.java
@@ -2,6 +2,8 @@ package unlam.edu.ar.pb2;
 
 import static org.junit.Assert.*;
 
+import java.util.TreeSet;
+
 import org.junit.Test;
 
 public class Testing {
@@ -38,6 +40,23 @@ public class Testing {
 		Figurita figurita = new Figurita("C", "Argentina", "Lionel Andres Messi", 100000, 10);
 		usuarioFinal.agregarFigurita(figurita);
 		assertEquals(1, usuarioFinal.getFiguritasAgregadas().size());
+	}
+	
+	@Test 
+	public void queLasFiguritasAgregadasDeFormaDesordenadaQuedenOrdenadas() {
+		Usuario administrador = new Administrador();
+		Figurita figurita = new Figurita("C", "Argentina", "Lionel Andres Messi", 100000, 10);
+		Figurita figurita2 = new Figurita("C", "Argentina", "Leopoldo", 100000, 11);
+		Figurita figurita1 = new Figurita("C", "Argentina", "El dibu", 100000, 1);
+		administrador.agregarFigurita(figurita);
+		administrador.agregarFigurita(figurita2);
+		administrador.agregarFigurita(figurita1);
+		TreeSet <Figurita> figuritas = new TreeSet<Figurita>(new OrdernarFiguritas()); 
+		figuritas.add(figurita1);
+		figuritas.add(figurita);
+		figuritas.add(figurita2);
+		
+		assertEquals(figuritas, administrador.getFiguritasAgregadas());
 	}
 	
 }
