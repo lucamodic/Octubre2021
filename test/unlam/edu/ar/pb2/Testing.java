d04955c98a219daceddcfc2986bb23a761a40f31
Luca <lucamodic17@gmail.com>
2022-11-07 09:56:47 -0300

    Primero

---
+++ test/unlam/edu/ar/pb2/Testing.java
@@ -0,0 +1,14 @@
+package unlam.edu.ar.pb2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
